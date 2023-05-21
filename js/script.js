9588c5d22bdb44779984ed93a1b82885f301087f
wavilches <wavilches@gmail.com>
2023-05-21 13:15:54 -0300

    entrega tarea clase 10 aapro23

---
+++ js/script.js
@@ -0,0 +1,31 @@
+document.write('Tarea Clase 10 - JavaScript')
+
+let nombre = 'Walter'
+let apellido = 'Vilches'
+
+console.log(nombre + ' ' + apellido);
+
+let num1 = 8;
+let num2 = 2;
+let suma = num1 + num2;
+let resta = num1 - num2;
+let multiplicacion = num1 * num2;
+let division = num1 / num2;
+
+console.log('Suma:', suma)
+console.log('Resta', resta)
+console.log('Multiplicación', multiplicacion)
+console.log('División', division)
+
+let num3 = 4;
+
+console.log('3er variable', suma + num3)
+
+
+
+
+
+
+
+
+
